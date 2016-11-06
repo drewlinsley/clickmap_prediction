add2893a0cf8a6921523da2d9a05245e7a566959
drewlinsley <drew@x9.clps.brown.edu>
2016-11-06 09:08:25 -0500

    added shuffle to fine tuning and fixed error with data generator

--- fine_tuning.py
+++ fine_tuning.py
@@ -16,12 +16,22 @@ from keras import backend as K
 from model import ml_net_model, loss
 
 
-def generator(b_s, images, maps, shape_r, shape_c, shape_r_gt, shape_c_gt):
+def generator(b_s, images, maps, shape_r, shape_c, shape_r_gt, shape_c_gt,shuffle=True):
+    if shuffle:
+        rand_order = np.arange(len(images))
+        np.random.shuffle(rand_order)
+	ar_images = np.asarray(images)
+        ar_maps = np.asarray(maps)
+        ar_images = ar_images[rand_order]
+        ar_maps = ar_maps[rand_order]
+        images = ar_images.tolist()
+        maps = ar_maps.tolist()        
     counter = 0
     while True:
+        if counter + b_s >= images:
+            break#% len(images)
         yield preprocess_images(images[counter:counter + b_s], shape_r, shape_c), preprocess_maps(maps[counter:counter + b_s], shape_r_gt, shape_c_gt)
-        counter = (counter + b_s) % len(images)
-
+        counter = (counter + b_s)
 
 def generator_test(b_s, images):
     counter = 0
@@ -88,13 +98,14 @@ def finetune_model(prog_path,nb_epoch,train_iters,val_iters,training_image_path,
     for ep in range(nb_epoch):
 	ep_loss = 0
 	num_batches = 0
-        batch_estimate = np.round(len(imgs_train_path) / b_s)
+        batch_estimate = int(np.ceil(len(imgs_train_path) / b_s))
         for X_train, Y_train in generator(b_s, imgs_train_path, maps_train_path, shape_r, shape_c, shape_r_gt, shape_c_gt):
             ep_loss += model.train_on_batch(X_train, Y_train)
             num_batches += 1
-	    print(str(num_batches) + '/' + str(batch_estimate))
+	    sys.stdout.write('\r' + str(num_batches) + '/' + str(batch_estimate))
+            sys.stdout.flush()
         print('mean loss across batches', ep_loss / num_batches)
-        model_pointer = timestamp + '/' + str(ep)
+        model_pointer = timestamp + '/' + str(ep) + '.h5'
         save_model(model,model_pointer)
     return model_pointer
 
